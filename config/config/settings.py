20f756ce7d21165119a16087563c6299c014c01d
Elian0 <>
2026-06-01 10:32:05 -0400

    Agregar Procfile

--- config/config/settings.py
+++ config/config/settings.py
@@ -69,6 +69,8 @@ USE_TZ        = True
 STATIC_URL  = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL           = '/'
